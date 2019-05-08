eb88bbd6da4bf9ab08717c81a45a369da72eafe1
Ayaan1 <Ayaan1@me.github.com>
2019-05-09 00:22:32 +0530

    thymeleaf parsing error due to typo .... be careful while writting template MVC started 02 Building a Spring Boot Web App 023 Thymeleaf Templates

---
+++ src/main/java/com/anki2bajaj/Spring5WebappStarter/Controller/BookController.java
@@ -0,0 +1,25 @@
+package com.anki2bajaj.Spring5WebappStarter.Controller;
+
+
+import com.anki2bajaj.Spring5WebappStarter.Repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){ // passing a model attribute which act as a transportor to view
+
+        model.addAttribute("book" , bookRepository.findAll());
+
+        return "books" ; // its just returning to view page named books       ie. books.html in view folder in resources
+                                // name can be any thing but that should be same in view folder
+    }
+}
