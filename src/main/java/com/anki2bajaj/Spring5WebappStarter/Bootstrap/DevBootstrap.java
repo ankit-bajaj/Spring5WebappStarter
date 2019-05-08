fa96b9fdf617d6e39fc5a91b366f3fa4bb166452
Ayaan1 <Ayaan1@me.github.com>
2019-05-08 21:54:27 +0530

    Initialising data for spring framework events 02 Building a Spring Boot Web App 018 course

--- src/main/java/com/anki2bajaj/Spring5WebappStarter/Bootstrap/DevBootstrap.java
+++ src/main/java/com/anki2bajaj/Spring5WebappStarter/Bootstrap/DevBootstrap.java
@@ -1,13 +1,24 @@
 package com.anki2bajaj.Spring5WebappStarter.Bootstrap;
 
+import com.anki2bajaj.Spring5WebappStarter.Repository.AuthorRepository;
+import com.anki2bajaj.Spring5WebappStarter.Repository.BookRepository;
 import com.anki2bajaj.Spring5WebappStarter.model.Author;
 import com.anki2bajaj.Spring5WebappStarter.model.Book;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
 
-
+@Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    public AuthorRepository authorRepository;
+    public BookRepository bookRepository ;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
@@ -22,9 +33,13 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
         Author rob = new Author("Rob" , "Johnas");
         Book noEJB = new Book("j2ee without ejb" , "1234", "Wrox");
         rob.getBooks().add(noEJB);
 
+        authorRepository.save(rob);
     }
 }
